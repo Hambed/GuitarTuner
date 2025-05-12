3bf279043aec63ee8797d7d028eb8fc6d644fb19
Hamilton Bird <hjwbirdprofessional@gmail.com>
2025-05-12 05:39:42 -0400

    Form for username and password
    taking input from user and putting it in the database on the pi.

---
+++ create_user.php
@@ -0,0 +1,66 @@
+<head?php
+
+if($_SERVER[REQUEST_METHOD"] == "POST)
+{
+    $username = trim($_POST["username"]);
+    $password = $_POST["password"];
+
+    //Check entry
+    if(empty(trim($username)) || empty($password))
+    {
+        echo"Username and Password are Required.";
+    }
+    
+    elseif(strlen($username) > 11)
+    {
+        echo "Username must be 11 Characters or Less";
+    }
+    else
+    {
+        //Hash the password
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+        //Connect to Database
+        $conn = new msqli("localhost", "php", "0s@48X+_tDL,E)cDC@n>9)UM7Lh:eY", "TunerDB");
+
+        if($conn->connect_error)
+        {
+            die("Connection Failed: " . $conn->connect_error);
+        }
+
+        //insert data to TunerDB
+        $stmt = $conn->prepare("INSERT INTO users (username, password_hash) VALUES (?, ?)");
+        $stmt->bind_param("ss", $username, $hashed_password);
+
+        if($stmt->execute())
+        {
+            echo "User Created Successfully.";
+        }
+        else
+        {
+            echo "Error: ". $stmnt->error;
+        }
+
+        $stmt->close();
+        $conn->close();
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Create User</title>
+</head>
+<body>
+    <h1>Create A New User</h1>
+    <form>
+        <label>Username:</label>
+        <input type="text" name="username" maxlength="11" required><br><br>
+        <label>Password:</label>
+        <input type="password" name="password" required><br><br>
+        <button type="submit">Create User</button>
+    </form>
+</body>
+</html>
